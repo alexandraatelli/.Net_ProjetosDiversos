dfa5ff1faf277737e37cb13ad7e471c8543613a8
Alexandra Carla Atelli <101141804+alexandraatelli@users.noreply.github.com>
2022-06-03 18:09:08 -0300

    Atualização

---
+++ RefatProdEstoque_Construtor/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Refatorando o exercício Produto Estoque_cs com uso de Construtores.
+ (* Fazer um programa que lê os dados de um produto e estoque (nome, preço e quantidade). Em seguida:
+ *  Mostrar os dados do produto (nome, preço, quantidade no estoque, valor total no estoque);
+ *  Realizar uma entrafa no estoque e mostrar novamente os dados do produto;
+ *  Realizar uma saída no estoque e mostrar novamente os dados do produto.)*/
+
+
+namespace RefatProdEstoque_Construtor.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciando o produto ele já inicia com nulo, zero...a depender do tipo
+            // dos atributos. Com o construtor o programador é obrigado a informar valores
+            // ao instanciar. Construtor vem logo após os atributos lá na classe RefatProdutos.
+            // RefatProduto p = new RefatProduto("TV, 500.0, 10"); //Tem que informar os parâmetros aqui () se não informa erro, ele tá pedindo os argumentos. O construtor obriga entrar com os dados.
+
+
+            // Aqui vou criar variáveis temporárias auxiliares, só para receber os dados, e somente
+            // que vamos chamar o construtor para instaciar o objeto:
+            Console.Write("Digite o nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite o preço do produto: ");
+            double preco = double.Parse(Console.ReadLine()); 
+            Console.Write("Digite quantidade do produto em estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            
+            RefatProduto p = new RefatProduto(nome, preco, quantidade);
+            // Recebe o nome, preço e quantidade que o usuario digitou, instancia o objeto já informando os dados para ele.
+            Console.WriteLine($"Dados do produto: {p}");
+
+
+            Console.Write("\nDigite o número de produtos a serem adicionados no estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            // Operação que é capaz de receber o valor de entrada e adicioná-lo na quantidade que já está armazenada na variável Quantidade do Produto p.
+            Console.WriteLine($"Dados do produto: {p}");
+
+            Console.Write("\nDigite o número de produtos a serem removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());// ela já foi declarada acima. Então não declara mais.
+            p.RemoverProdutos(qte);
+            // Operação que é capaz de remover a quantidade deseja do Produto p, que está armazenada na variável Quantidade do Produto p.
+            Console.WriteLine($"Dados do produto: {p}");
+        }
+    }
+}
+          
---
+++ RefatProdEstoque_Construtor/RefatProduto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefatProdEstoque_Construtor.cs
+{
+    internal class RefatProduto // após instanciar no programa principal, o construtor vem logo após os atributos. Ele inicia valores nas variáveis.
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //O construtor tem o mesmo nome da classe. É uma operação que tem mesmo nome da Classe. Incia com public. Podemos informar se tem algum parâmetro de entrada. Aqui no exemplo temos (parametros, inicia com letra minuscula)
+        // Isso significa que quando instanciar já é obrigado a informar os atributos acima no Construtor.
+        // Com os dados informados no construtor, atribui-se a cada atributo da classe.
+        public RefatProduto (string nome, double preco, int quantidade)
+        {
+            Nome = nome; // Nome maiúsculo do atributo do objeto, vai receber nome em minúsculo, que é o dado que chegou de entrada no construtor.
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        // valor total em estoque:
+        public double ValorTotalEmEstoque()
+        {   // retorna como saída um valor preço * quantidade
+            return Preco * Quantidade;
+        }
+        // É void(vazio) pois não irá retornar nada, só vai Quantidade e acrescentará produto no estoque.
+        public void AdicionarProdutos(int quantidade) // Essa operação vai precisar receber um dado de entrada, que é a quantidade a ser adicionada no estoque. Quando vai colocar alguma variável como parâmetro de entrada de função, inicia-se com letra minúscula - convenção.)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+        public void RemoverProdutos(int quantidade)//Vamos remover produtos do estoque
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        // Faremos uma sobreposição:
+        public override string ToString()
+        {
+            return Nome
+                + ", R$ "
+                + Preco.ToString("F2")
+                + ", "
+                + Quantidade
+                + " unidade, Total: R$ "
+                + ValorTotalEmEstoque().ToString("F2");
+        }
+    }
+}
